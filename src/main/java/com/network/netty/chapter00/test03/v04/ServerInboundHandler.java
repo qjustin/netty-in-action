8502519520891a8257ddb066f941625da152961d
jun.hu <jun.hu@picahealth.com>
2019-06-28 17:33:02 +0800

    'chagnes'

--- src/main/java/com/network/netty/chapter00/test03/v04/ServerInboundHandler.java
+++ src/main/java/com/network/netty/chapter00/test03/v04/ServerInboundHandler.java
@@ -4,6 +4,8 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -19,7 +21,16 @@ public class ServerInboundHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception {
-        channels.writeAndFlush("[" + ctx.channel().remoteAddress() + "]" + s + "\r\n");
+        channels.writeAndFlush("[" + ctx.channel().remoteAddress() + "]" + s + "\r\n").addListener(
+                new GenericFutureListener<Future<? super Void>>() {
+                    @Override
+                    public void operationComplete(Future<? super Void> future) throws Exception {
+                        if (future.isSuccess()) {
+                            System.out.println("boardcast finished : " + s);
+                        }
+                    }
+                }
+        );
         System.out.println("server boardcast message '" + s + "' to " + connCounts.get() + " clients.");
     }
 
