8ee4163d8aa82b858be744d57ab865117b9e49cf
jun.hu <jun.hu@picahealth.com>
2019-07-08 14:34:33 +0800

    'chagnes'

---
+++ src/main/java/com/network/netty/chapter00/test04/Client.java
@@ -0,0 +1,55 @@
+package com.network.netty.chapter00.test04;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+
+public class Client {
+    static final ByteBuf message = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("client say hello server\r\n", CharsetUtil.UTF_8));
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap boot = new Bootstrap();
+            boot.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel channel) throws Exception {
+                            channel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    ctx.writeAndFlush(message);
+                                }
+
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    System.out.println("client receive: " + ((ByteBuf)msg).toString(CharsetUtil.UTF_8));
+                                    ctx.writeAndFlush(message);
+                                }
+                            });
+                        }
+                    });
+
+            ChannelFuture future = boot.connect("localhost", 1127).sync();
+            future.channel().closeFuture().addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    System.out.println("Connected to server : localhosts:1127");
+                }
+            });
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
